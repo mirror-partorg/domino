99bfff2eaac8de7f63b4ebf3815f441decdbfcf7
mirror-partorg <mir@glekar.com>
2019-11-06 20:41:19 +0700

    python 3.8 win64 1

---
+++ domino/lib/pydomino/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup, Extension
+
+import os
+
+files = ["var/"+f for f in  os.listdir("var") if f[-2:] == ".c"]
+files = files + ["src/"+f for f in  os.listdir("src") if f[-2:] == ".c"]
+#files = files + [f for f in  os.listdir(".") if f[-2:] == ".c"]
+print(files)
+
+setup(
+    version='1.1.0',
+    name='pydomino',
+    ext_modules=[
+        Extension(
+            'pydomino',
+            files,
+            include_dirs = ["../../include","../../var/MINGW64_NT-6.3-9600/x86_64"],
+	    libraries=['dolib', 'domino'],
+            library_dirs=['../../var/MSYS_NT-6.3-9600/x86_64/.libs'],
+            define_macros=[("BTI_WIN_64",None),]
+        )
+    ],
+)
